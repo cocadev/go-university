dd272f35a692f72ee1bbbaa59f18180ade356a7d
Wang Harry <eugene19950901@outlook.com>
2019-06-27 02:20:46 +0800

    switch-combine-cases

--- condition.go
+++ condition.go
@@ -46,22 +46,32 @@ func main() {
 	// }
 
 	//====================== 4
-	var dayOfWeek = 6
-	switch dayOfWeek {
-		case 1: fmt.Println("Monday")
-		case 2: fmt.Println("Tuesday")
-		case 3: fmt.Println("Wednesday")
-		case 4: fmt.Println("Thursday")
-		case 5: fmt.Println("Friday")
-		case 6: {
-			fmt.Println("Saturday")
-			fmt.Println("Weekend. Yaay!")
-		}
-		case 7: {
-			fmt.Println("Sunday")
-			fmt.Println("Weekend. Yaay!")
-		}
-		default: fmt.Println("Invalid day")
+	// var dayOfWeek = 6
+	// switch dayOfWeek {
+	// 	case 1: fmt.Println("Monday")
+	// 	case 2: fmt.Println("Tuesday")
+	// 	case 3: fmt.Println("Wednesday")
+	// 	case 4: fmt.Println("Thursday")
+	// 	case 5: fmt.Println("Friday")
+	// 	case 6: {
+	// 		fmt.Println("Saturday")
+	// 		fmt.Println("Weekend. Yaay!")
+	// 	}
+	// 	case 7: {
+	// 		fmt.Println("Sunday")
+	// 		fmt.Println("Weekend. Yaay!")
+	// 	}
+	// 	default: fmt.Println("Invalid day")
+	// }
+
+	//======================= 5
+	switch dayOfWeek := 5; dayOfWeek {
+	case 1, 2, 3, 4, 5:
+		fmt.Println("Weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Invalid Day")		
 	}
 }
 	
