d7ef13b4ff1ae7b284ba80f68fee7e9a2e09e7c1
Wang Harry <eugene19950901@outlook.com>
2019-06-27 01:48:43 +0800

    type-inference

--- main.go
+++ main.go
@@ -3,31 +3,46 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+
+
+	//================================ 1
+
+	//fmt.Println("Hello, World")
+
+	//================================ 2
+
 	// Declaring Variables
-	var myStr string = "Hello"
-	var myInt int = 100
-	var myFloat float64 = 45.12
-	fmt.Println(myStr, myInt, myFloat)
-
-
-	//================================
+	// var myStr string = "Hello"
+	// var myInt int = 100
+	// var myFloat float64 = 45.12
+	//fmt.Println(myStr, myInt, myFloat)
 
 
 	// Multiple Declarations
-	var (
-		employeeId int = 5
-		firstName, lastName string = "Satoshi", "Nakamoto"
-	)
-	fmt.Println(employeeId, firstName, lastName)
+	// var (
+	// 	employeeId int = 5
+	// 	firstName, lastName string = "Satoshi", "Nakamoto"
+	// )
+	//fmt.Println(employeeId, firstName, lastName)
 
+	
+	// Short variable declaration syntax
+	// name := "Rajeev Singh"
+	// age, salary, isProgrammer := 35, 50000.0, true
+
+	//fmt.Println(name, age, salary, isProgrammer)
+
+
+	//================================ 3
+	var name = "Rajeev Singh" // Type declaration is optional here.
+	fmt.Printf("Variable 'name' is of type %T\n", name)
 
 	//================================
 
+	// Multiple variable declarations with inferred types
+	var firstName, lastName, age, salary = "John", "Maxwell", 28, 50000.0
 
-	// Short variable declaration syntax
-	name := "Rajeev Singh"
-	age, salary, isProgrammer := 35, 50000.0, true
+	fmt.Printf("firstName: %T, lastName: %T, age: %T, salary: %T\n",
+		firstName, lastName, age, salary)
 
-	fmt.Println(name, age, salary, isProgrammer)
 }
