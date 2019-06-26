587da40260780bfc18dd669b4b8130092afacee9
Wang Harry <eugene19950901@outlook.com>
2019-06-27 02:19:49 +0800

    switch-statement

--- condition.go
+++ condition.go
@@ -34,15 +34,34 @@ func main() {
 	// }
 
 	//======================3
-	var BMI = 21.0
-	if BMI < 18.5 {
-		fmt.Println("You are underweight");
-	} else if BMI >= 18.5 && BMI < 25.0 {
-		fmt.Println("Your weight is normal");
-	} else if BMI >= 25.0 && BMI < 30.0 {
-		fmt.Println("You're overweight")
-	} else {
-		fmt.Println("You're obese")
+	// var BMI = 21.0
+	// if BMI < 18.5 {
+	// 	fmt.Println("You are underweight");
+	// } else if BMI >= 18.5 && BMI < 25.0 {
+	// 	fmt.Println("Your weight is normal");
+	// } else if BMI >= 25.0 && BMI < 30.0 {
+	// 	fmt.Println("You're overweight")
+	// } else {
+	// 	fmt.Println("You're obese")
+	// }
+
+	//====================== 4
+	var dayOfWeek = 6
+	switch dayOfWeek {
+		case 1: fmt.Println("Monday")
+		case 2: fmt.Println("Tuesday")
+		case 3: fmt.Println("Wednesday")
+		case 4: fmt.Println("Thursday")
+		case 5: fmt.Println("Friday")
+		case 6: {
+			fmt.Println("Saturday")
+			fmt.Println("Weekend. Yaay!")
+		}
+		case 7: {
+			fmt.Println("Sunday")
+			fmt.Println("Weekend. Yaay!")
+		}
+		default: fmt.Println("Invalid day")
 	}
 }
 	
