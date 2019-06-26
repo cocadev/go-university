5f654cfd6811430a7c07d116df67742e190b2e3b
Wang Harry <eugene19950901@outlook.com>
2019-06-27 02:02:14 +0800

    numeric-operations

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+	 "math"
+	)
 
 func main() {
 
@@ -77,9 +80,24 @@ func main() {
 	// fmt.Printf("%d, %d, %d, %#x, %#o %f %f\n", myInt8, myInt, myUint, myHexNumber, myOctalNumber, myFloat32, myFloat)
 
 	//========= 6
-	var myByte byte = 'a'
-	var myRune rune = '♥'
+	// var myByte byte = 'a'
+	// var myRune rune = '♥'
 
-	fmt.Printf("%c = %d and %c = %U\n", myByte, myByte, myRune, myRune)
+	// fmt.Printf("%c = %d and %c = %U\n", myByte, myByte, myRune, myRune)
+
+	//========= 7
+	var a, b = 4, 5
+	var res1 = (a + b) * (a + b)/2  // Arithmetic operations
+
+	a++ // Increment a by 1
+
+	b += 10 // Increment b by 10
+
+	var res2 = a ^ b // Bitwise XOR
+
+	var r = 3.5
+	var res3 = math.Pi * r * r  // Operations on floating-point type
+
+	fmt.Printf("res1 : %v, res2 : %v, res3 : %v\n", res1, res2, res3)
 
 }
