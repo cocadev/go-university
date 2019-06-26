bb6d9cb68117fe40dd35c7bc4d47fb2c64bbacc1
Wang Harry <eugene19950901@outlook.com>
2019-06-27 02:16:11 +0800

    if-else

--- condition.go
+++ condition.go
@@ -2,27 +2,35 @@ package main
 import "fmt"
 
 func main() {
-	// If Statement
-	var x = 25
-	if(x % 5 == 0) {
-		fmt.Printf("%d is a multiple of 5\n", x)
-	}
+	// // If Statement
+	// var x = 25
+	// if(x % 5 == 0) {
+	// 	fmt.Printf("%d is a multiple of 5\n", x)
+	// }
 
-	// Parentheses are Optional
-	var y = -1
-	if y < 0 {
-		fmt.Printf("%d is negative\n", y)
-	}
+	// // Parentheses are Optional
+	// var y = -1
+	// if y < 0 {
+	// 	fmt.Printf("%d is negative\n", y)
+	// }
 	
-	// If with a condition consisting of short circuit operators
-	var age = 21
-	if age >= 17 && age <= 30 {
-		fmt.Println("My Age is between 17 and 30")
-	}	
+	// // If with a condition consisting of short circuit operators
+	// var age = 21
+	// if age >= 17 && age <= 30 {
+	// 	fmt.Println("My Age is between 17 and 30")
+	// }	
 
-	// If with a short statement	
-	if n := 10; n%2 == 0 {
-		fmt.Printf("%d is even\n", n)
-	} 
-	
+	// // If with a short statement	
+	// if n := 10; n%2 == 0 {
+	// 	fmt.Printf("%d is even\n", n)
+	// } 
+
+	//=====================2
+	var age = 18
+	if age >= 18 {
+		fmt.Println("You're eligible to vote!")
+	} else {
+		fmt.Println("You're not eligible to vote!")
+	}
 }
+	
