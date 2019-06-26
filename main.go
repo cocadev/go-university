7049c0b3bebf0cd477f1c30bd06245a42caa2c81
Wang Harry <eugene19950901@outlook.com>
2019-06-27 01:51:33 +0800

    numeric-types

--- main.go
+++ main.go
@@ -57,4 +57,23 @@ func main() {
 	fmt.Printf("firstName: %s, lastName: %s, age: %d, salary: %f, isConfirmed: %t\n",
 		firstName, lastName, age, salary, isConfirmed)
 
+	//================================== 5
+	var myInt8 int8 = 97
+
+	/*
+	  When you don't declare any type explicitly, the type inferred is `int`
+	  (The default type for integers)
+	*/
+	var myInt = 1200
+
+	var myUint uint = 500
+
+	var myHexNumber = 0xFF  // Use prefix '0x' or '0X' for declaring hexadecimal numbers
+	var myOctalNumber = 034 // Use prefix '0' for declaring octal numbers
+
+	var myFloat32 float32 = 4.5
+	var myFloat = 9.12 // // Type inferred as `float64` (the default type for floating-point numbers)
+
+	fmt.Printf("%d, %d, %d, %#x, %#o %f %f\n", myInt8, myInt, myUint, myHexNumber, myOctalNumber, myFloat32, myFloat)
+
 }
