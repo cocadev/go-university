fc1b70046cf3150276a21a0eaf4ae4a274af02b7
Wang Harry <eugene19950901@outlook.com>
2019-06-27 01:50:02 +0800

    zero-values

--- main.go
+++ main.go
@@ -34,15 +34,27 @@ func main() {
 
 
 	//================================ 3
-	var name = "Rajeev Singh" // Type declaration is optional here.
-	fmt.Printf("Variable 'name' is of type %T\n", name)
+	// var name = "Rajeev Singh" // Type declaration is optional here.
+	// fmt.Printf("Variable 'name' is of type %T\n", name)
 
-	//================================
+	// //================================
 
-	// Multiple variable declarations with inferred types
-	var firstName, lastName, age, salary = "John", "Maxwell", 28, 50000.0
+	// // Multiple variable declarations with inferred types
+	// var firstName, lastName, age, salary = "John", "Maxwell", 28, 50000.0
 
-	fmt.Printf("firstName: %T, lastName: %T, age: %T, salary: %T\n",
-		firstName, lastName, age, salary)
+	// fmt.Printf("firstName: %T, lastName: %T, age: %T, salary: %T\n",
+	// 	firstName, lastName, age, salary)
+
+	//================================= 4
+
+	var (
+		firstName, lastName string
+		age                 int
+		salary              float64
+		isConfirmed         bool
+	)
+
+	fmt.Printf("firstName: %s, lastName: %s, age: %d, salary: %f, isConfirmed: %t\n",
+		firstName, lastName, age, salary, isConfirmed)
 
 }
